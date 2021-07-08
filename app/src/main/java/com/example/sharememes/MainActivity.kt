ab8e60579f1edab24c21330b27182534e16cc9b2
Abhaykulshrestha13 <abhaykul2001@gmail.com>
2021-07-08 16:55:23 +0530

    Added ShareMeme App

--- app/src/main/java/com/example/sharememes/MainActivity.kt
+++ app/src/main/java/com/example/sharememes/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadMeme() {
         progressBar.visibility = View.VISIBLE
-        val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
 
         // Request a string response from the provided URL.
@@ -67,7 +66,7 @@ class MainActivity : AppCompatActivity() {
             })
 
         // Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest)
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
     fun ShareMeme(view: View) {
